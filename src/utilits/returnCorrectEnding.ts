b7642807669bbe7322ec316c591b5943376879da
Artem Lyamzin <artemlyamzin@redcollar.ru>
2022-12-13 13:41:29 +0600

    endings

--- src/utilits/returnCorrectEnding.ts
+++ src/utilits/returnCorrectEnding.ts
@@ -1,4 +1,5 @@
 export const returnCorrectEnding = (q: number) => {
+    if (q === 11 || q === 12 || q === 13) return "Персонажей"
     const stringQArr = q.toString().split('');
     const lastNumber = Number(stringQArr[stringQArr.length - 1]);
     switch (lastNumber) {
